e0c5feabfa154e680a77c9c776a0c7581f5a0813
ryoogata <ryo.ogata@gmail.com>
2017-01-05 11:19:01 +0900

    first commit

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "subnet1a_DMZ_id" {
+  value = "${aws_subnet.subnet1a_DMZ.id}"
+}
+
+output "subnet1c_DMZ_id" {
+  value = "${aws_subnet.subnet1c_DMZ.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_block" { } 
+variable "vpc_subnet_1a_DMZ" { } 
+variable "vpc_subnet_1c_DMZ" { } 
+variable "tag" { } 
